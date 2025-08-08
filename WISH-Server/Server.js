ee13972d526221f6d0e158b6565a2b209d78460e
endiy0 <steve060401@gmail.com>
2025-08-08 21:36:50 +0900

    rollback port to 80

--- WISH-Server/Server.js
+++ WISH-Server/Server.js
@@ -2,7 +2,7 @@ const express = require("express");
 //const { v4: uuidv4 } = require("uuid");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 4000;
+const port = 80;
 
 app.use(express.static("public"));
 app.use(express.json());
